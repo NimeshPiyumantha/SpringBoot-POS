4a6ae881fea55c4b510aed04b36dfbd0ce680929
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-07 17:56:33 +0530

    Refactor Project Spring Boot

--- POS_BackEnd/src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ POS_BackEnd/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -1,26 +1,23 @@
 package lk.ijse.spring;
 
-import lk.ijse.spring.config.WebAppConfig;
-import lk.ijse.spring.config.WebRootConfig;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{WebRootConfig.class};
+@SpringBootApplication
+public class WebAppInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppInitializer.class);
     }
 
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{WebAppConfig.class};
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
